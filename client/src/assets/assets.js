7c64d54e39b2d2aeb31ea2ad38dc2311e266ec65
vamshi5654 <bandarivamshi07@gmail.com>
2026-05-17 11:41:44 +0530

    updated logo

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import logo1 from "./logo1.svg"
 import search_icon from "./search_icon.svg";
 import company_icon from "./company_icon.svg";
